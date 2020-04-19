efd73fa6e216b71029babdf98132355613f632e6
David <thedavid1337@gmail.com>
2020-04-19 22:16:40 +0300

    added context api and redcuer

--- tic-tac-toe-client/src/App.js
+++ tic-tac-toe-client/src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import './App.css';
+import { useImmerReducer } from 'use-immer';
+import { loginReducer } from './reducers/LoginReducer';
+import { initialState } from './store/InitialState';
+import { StateContext, DispatchContext } from './context/StateContext';
+import Lobby from './components/Lobby';
+import Login from './components/Login';
 
 function App() {
+
+  const [state, dispatch] = useImmerReducer(loginReducer, initialState);
+
+  const { isLoggedIn } = state;
+
   return (
-    <div className="App">
-      <h2>Hello~!!!!</h2>
-    </div>
+    <StateContext.Provider value={state}>
+      <DispatchContext.Provider value={dispatch}>
+        <>
+          { isLoggedIn ? <Lobby /> : <Login /> }
+        </>
+      </DispatchContext.Provider>
+    </StateContext.Provider>
   );
 }
 
