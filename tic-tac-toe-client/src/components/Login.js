60d2f170d0ed37bbd51d3358a1df2996c823b374
david.c <david.c@compie.co.il>
2020-04-20 22:32:48 +0300

    added login html

--- tic-tac-toe-client/src/components/Login.js
+++ tic-tac-toe-client/src/components/Login.js
@@ -1,11 +1,52 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import {DispatchContext, StateContext} from "../context/StateContext";
 
 export default function Login() {
 
+    const dispatch = useContext(DispatchContext);
+    const state = useContext(StateContext);
+
+    const { username, password, isLoading, error } = state;
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        console.log('TEST?');
+    }
 
     return (
-        <div>
-            <h2>Login Page</h2>
-        </div>
+        <>
+            <form className="form" onSubmit={onSubmit}>
+                {error && <p className="error">{error}</p>}
+                <p>Please Login!</p>
+                <input
+                    type="text"
+                    placeholder="username"
+                    value={username}
+                    onChange={e =>
+                        dispatch({
+                            type: 'field',
+                            fieldName: 'username',
+                            payload: e.currentTarget.value,
+                        })
+                    }
+                />
+                <input
+                    type="password"
+                    placeholder="password"
+                    autoComplete="new-password"
+                    value={password}
+                    onChange={e =>
+                        dispatch({
+                            type: 'field',
+                            fieldName: 'password',
+                            payload: e.currentTarget.value,
+                        })
+                    }
+                />
+                <button className="submit" type="submit" disabled={isLoading}>
+                    {isLoading ? 'Logging in...' : 'Log In'}
+                </button>
+            </form>
+        </>
     )
 }
